4177f5ba1d0d948ff6d8f51da1d7c2dacaa53fb5
daniel <yoojeenkim@gmail.com>
2021-11-15 16:44:08 -0500

    added indexdb

---
+++ public/db.js
@@ -0,0 +1,43 @@
+function checkIndexDb() {
+    if (!window.indexedDB) {
+        return false;
+    }
+
+    const request = window.indexedDB.open("budget", 1);
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    let db,
+    tx,
+    store;
+
+    request.onupgradeneeded = ({ target }) => {
+        db = target.result;
+        db.createObjectStore("pending", { autoIncrement: true });
+    };
+
+    request.onerror = ({ target }) => {
+        console.log("There was an error!");
+    };
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            })
+        }
+    }
+}
--- public/index.js
+++ public/index.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+  });
+}
+
 let transactions = [];
 let myChart;
 
