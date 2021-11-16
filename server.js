e4b9b9cf9fc7f4e4fa70145aba685dfa58d563d6
daniel <yoojeenkim@gmail.com>
2021-11-15 20:14:57 -0500

    deploy fixed mongoose connect in server js

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-    useNewUrlParser: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/budget",
+    {
+        useNewUrlParser: true
+    }
+);
 
 app.use(require("./routes/api.js"));
 
